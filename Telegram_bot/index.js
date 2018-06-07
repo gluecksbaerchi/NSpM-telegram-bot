65bb43ee454fb0f99821da2a820d8d3083348903
rzingler <ronny.zingler@stud.htwk-leipzig.de>
2018-06-07 08:33:43 +0200

    bot on message request an nspm

--- Telegram_bot/index.js
+++ Telegram_bot/index.js
@@ -1,24 +1,43 @@
 process.env["NTBA_FIX_319"] = 1;
 
 var express = require('express');
-var bodyParser = require('body-parser');
 const token = '491825766:AAFEJ0vsOmEOBJr_KRJOcY9OWCweuIqQgzo';
-const ngrok = require('ngrok');
+const http = require('http');
 
 var app = express();
 
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(token, {polling: true});
 
-bot.on('message', (msg) => {
-
-    var Hi = "hi";
-    if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
-        bot.sendMessage(msg.chat.id,"Hello dear user");
+var post_options = {
+    host: 'nspm',
+    port: '5000',
+    path: '/webhook',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
     }
+};
+
+
+
+var post_data = '{"result": {"parameters": {"question": "is carew cross a place"}}}';
+
+bot.on('message', (msg) => {
+    var post_req = http.request(post_options, function(res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            var json = JSON.parse(chunk);
+            bot.sendMessage(msg.chat.id,json.displayText);
+        });
+    });
+
+    post_req.write(post_data);
+    post_req.end();
+    console.log('huhu');
 
 });
 
 app.listen(8080, function () {
-    console.log('Example app listening on port 8080!');
+
 });
