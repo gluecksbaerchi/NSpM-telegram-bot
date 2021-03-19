afb5ecb06852a29ff91fe5f7dd83ebb71625ad3f
ashutosh16399 <ashutosh16399@gmail.com>
2021-03-19 19:13:48 +0530

    python 3 update with contrib error corrected

--- NSpM-project/interpreter.py
+++ NSpM-project/interpreter.py
@@ -11,13 +11,13 @@ Version 0.1.0-akaha
 
 """
 import sys
-
+from importlib import reload
 from generator_utils import decode
 
 if __name__ == '__main__':
     reload(sys)
-    sys.setdefaultencoding("utf-8")
+    #sys.setdefaultencoding("utf-8")
 
     encoded_sparql = sys.argv[1]
     decoded_sparql = decode(encoded_sparql)
-    print decoded_sparql
+    print(decoded_sparql)
